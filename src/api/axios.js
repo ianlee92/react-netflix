8cbf2b3c6528eef0ef751fcdfb1e0b45d1867b72
ianlee92 <ethian9227@gmail.com>
2022-08-03 21:21:32 +0900

    api key 숨김

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-
-const API_KEY = process.env.THE_MOVIE_DB_KEY;
+import apiKey from "./apiKey";
 
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: API_KEY,
+    api_key: apiKey.movie,
     language: "ko-KR",
   },
 });
