574a2ff07134b6c73aa3a3c745ac611da27d1764
ianlee92 <ethian9227@gmail.com>
2022-08-03 21:49:58 +0900

    100자 이상 넘어갈 경우 substr로 자르기

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -21,7 +21,11 @@ function Banner() {
     });
     setMovie(movieDetail);
   };
-  console.log(movie);
+
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
+
   return (
     <header
       className="banner"
@@ -39,7 +43,7 @@ function Banner() {
           <button className="banner__button play">Play</button>
           <button className="banner__button info">More Information</button>
         </div>
-        <h1 className="banner__description">{movie.overview}</h1>
+        <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
       </div>
       <div className="banner--fadeBottom" />
     </header>
