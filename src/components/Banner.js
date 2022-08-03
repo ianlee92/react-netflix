731712f19261299f459f66b39e9de259c92f980f
ianlee92 <ethian9227@gmail.com>
2022-08-03 22:47:12 +0900

    Play 버튼 클릭시 구분

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,6 +5,8 @@ import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -26,28 +28,39 @@ function Banner() {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   };
 
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-        backgroundPosition: "top center",
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="banner__contents">
-        <h1 className="banner__title">
-          {movie.title || movie.name || movie.original_name}
-        </h1>
-        <div className="banner__buttons">
-          <button className="banner__button play">Play</button>
-          <button className="banner__button info">More Information</button>
+  if (!isClicked) {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie.title || movie.name || movie.original_name}
+          </h1>
+          <div className="banner__buttons">
+            <button
+              className="banner__button play"
+              onClick={() => setIsClicked(true)}
+            >
+              Play
+            </button>
+            <button className="banner__button info">More Information</button>
+          </div>
+          <h1 className="banner__description">
+            {truncate(movie.overview, 100)}
+          </h1>
         </div>
-        <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
-      </div>
-      <div className="banner--fadeBottom" />
-    </header>
-  );
+        <div className="banner--fadeBottom" />
+      </header>
+    );
+  } else {
+    return <div>Clicked</div>;
+  }
 }
 
 export default Banner;
